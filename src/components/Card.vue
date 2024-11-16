7cf8b3055fa6d5b4ef4113113efadacd098f1294
Mark Ciril <markciril.jamon@gmail.com>
2024-11-16 17:28:34 +0800

    Feat: create a templated for card that will use for the mini boxes in the main form

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="rounded-md border border-gray-400 w-full" :class="[height, minheight, maxheight]">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    height: {
+        type: String,
+        default: 'h-12'
+    },
+    minheight: {
+        type: String,
+        default: 'min-h-12'
+    },
+    maxheight: {
+        type: String,
+        default: 'max-h-24'
+    }
+});
+</script>
